d0bc11f27fda071b7a795f25d80e304333ea4a97
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-10-13 19:30:24 +0200

    Added --negate option

--- extendedDuplicateFinder.py
+++ extendedDuplicateFinder.py
@@ -30,7 +30,7 @@ def check_key(key_list, items):
 
 def gen_keylist(opts):
 	""" Generates list of all Item() attributes """
-	return [k for k in Item().keys() if (k in opts and opts[k]) or opts["compare_all"]]
+	return [k for k in Item().keys() if (bool(opts["negate_all_options"]) ^ bool(opts[k])) or opts["compare_all"]]
 
 def gen_parser(cmd):
 	""" Creates cmdline argument parser which contains each Item() attribute and a couple of additional parameters """
@@ -41,10 +41,16 @@ def gen_parser(cmd):
 		help = 'Not only list, but actually erase items from database and disk'
 	)
 	cmd.parser.add_option(
+		'--negate',
+		dest = 'negate_all_options',
+		action = 'store_true',
+		help = 'Negate all passed options, e.g. "--title --negate" would compare everything but the title (has no effect with --all)'
+	)
+	cmd.parser.add_option(
 		'--all',
 		dest = 'compare_all',
 		action = 'store_true',
-		help = 'Compares all available options, i.e. only finds exact matches'
+		help = 'Compares all available options, i.e. only finds exact matches (takes precedence over --negate)'
 	)
 
 	for k in Item().keys():
