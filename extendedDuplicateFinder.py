7fb60700bf7a4169108f4c7866509a2354270843
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-10-13 02:42:41 +0200

    Shortened code

--- extendedDuplicateFinder.py
+++ extendedDuplicateFinder.py
@@ -11,27 +11,15 @@ def check_key(key_list, items):
 	""" Creates dictionary which contains the matching attributes (as the key) and a list of the concerned items (as the value) """
 	matches = {}
 	for i in items:
-		res = []
-		for key in key_list:
-			cur = i[key]
-			if isinstance(cur, (int, long, float, complex)): # TODO: make this more elegant
-				res.append(str(cur))
-			else:
-				res.append(cur.encode('utf8'))
-		matches.setdefault("-".join(res), []).append(i)
+		matches.setdefault("-".join([str(i[key]) if isinstance(i[key], (int, long, float, complex)) else i[key].encode('utf8') for key in key_list]), []).append(i)
 	return matches
 
 def gen_keylist(opts):
 	""" Generates list of all Item() attributes """
-	out = []
-	for k in Item().keys():
-		if k in opts and opts[k]:
-			out.append(k)
-	return out
+	return [k for k in Item().keys() if k in opts and opts[k]]
 
 def gen_parser(cmd):
 	""" Creates cmdline argument parser which contains each Item() attribute and a couple of additional parameters """
-
 	cmd.parser.add_option(
 		'--delete',
 		dest = 'actually_delete_files',
